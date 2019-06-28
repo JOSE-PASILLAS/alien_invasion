d0efa3d7c544ec944a7f82d87168267ba4992bae
jose pasillas <alfa2plus@hotmail.com>
2019-06-27 23:41:05 -0400

    Functionality to simply move the ship

---
+++ alien_invasion.py
@@ -0,0 +1,31 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Initialize game and create a screen object.
+    pygame.init()
+
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
+
+    ship = Ship(screen)
+    # Start the main loop for the game.
+    while True:
+        #Watch for keyboard and mouse events.
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+
+
+
+run_game()
