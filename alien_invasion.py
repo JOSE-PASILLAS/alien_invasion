70e18524272ed06ec026c7f95d33eb8b179179fd
jose pasillas <alfa2plus@hotmail.com>
2020-01-09 18:06:42 -0500

    Improved version

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,31 +1,48 @@
-import sys
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
+from pygame.sprite import Group
+from alien import Alien
 import game_functions as gf
 
 
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)
 
-    ship = Ship(screen)
-    # Start the main loop for the game.
+    # Make the play button
+    play_button = Button(ai_settings, screen, "Play")
+    # Create an instance to store game statistics and store a scoreboard
+    stats = GameStats(ai_settings)
+    sb =  Scoreboard(ai_settings, screen, stats)
+    # Make a ship
+    ship = Ship(ai_settings,screen)
+    # Make a group to store bullets in
+    bullets = Group()
+    # Make a group of aliens
+    aliens = Group()
+
+    # Create a fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    alien = Alien(ai_settings, screen)
+
     while True:
-        #Watch for keyboard and mouse events.
-        gf.check_events(ship)
-        ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens,
+                        bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb,  ship, aliens, bullets)
 
-
-
-
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
